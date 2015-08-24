f548366d10eed6fd15291e7e33d6bd9a5c405ab1
Oleksandr Motsak <http://goo.gl/mcpzY>
2015-08-24 20:03:46 +0000

    Test Singular binding only if Cxx is available

--- test/Rings-test.jl
+++ test/Rings-test.jl
@@ -13,6 +13,18 @@ include("nmod_poly-test.jl")
 
 include("Fields-test.jl")
 
+function test_pkg_status(pkg)
+   tempiobuffer = IOBuffer(); 
+   Pkg.status(pkg, tempiobuffer);
+   return (tempiobuffer.size != 0);
+end
+   
+if( test_pkg_status("Cxx") )
+   include("singular-test.jl");
+else
+   test_singular() = println("\n\nIt appears that package 'Cxx' required for wrapping Singular (C++ code!) is missing! :(");
+end
+
 function test_rings()
    test_zz()
    test_poly()
@@ -25,6 +37,7 @@ end
 function test_all()
    test_rings()
    test_fields()
+   test_singular()
 end
 
 end # module
