bb0eaea2c2a99fe5bd9d057168ef1b2c2bb6f390
Oleksandr Motsak <http://goo.gl/mcpzY>
2015-08-24 21:33:02 +0000

    Added building of NTL 9.3.0 for Singular

--- deps/buildSingular.jl
+++ deps/buildSingular.jl
@@ -1,4 +1,4 @@
-function install_singular()
+function install_singular() # and NTL 9-3-0
 
 const oldwdir = pwd()
 const pkgdir = Pkg.dir("Nemo") 
@@ -23,6 +23,28 @@ else
    vdir = "$pkgdir/local"
 end
 
+cd(wdir)
+const ntl="ntl-9.3.0"
+
+try
+  run(`wget -q -nc -c -O "$wdir/$ntl.tar.gz" "http://www.shoup.net/ntl/$ntl.tar.gz"`)
+except
+end
+
+const tmp = mktempdir(wdir)
+
+# http://www.shoup.net/ntl/WinNTL-9_3_0.zip # under Windows?
+if !on_windows
+   cd( tmp )
+   run(`tar -C "$tmp" -xkvf "$wdir/$ntl.tar.gz"`)
+#   run(`rm $ntl.tar.gz`) ### avoid redownloading the same tarball...?
+   cd( joinpath(tmp, ntl, "src") )
+   run(`./configure DEF_PREFIX="$vdir" SHARED=on NTL_THREADS=off NTL_EXCEPTIONS=off NTL_GMP_LIP=on CXXFLAGS="-I$vdir/include" LDFLAGS="-L$vdir/lib"`)
+   run(`make -j4`)
+   run(`make install`)
+##   run(`rm -Rf $tmp/$ntl`)
+end
+
 
 ## Install Singular
 const srcs = joinpath(wdir, "Singular")
@@ -36,14 +58,12 @@ end
 
 run(`$srcs/autogen.sh`)
 
-# out of source-tree building:
-const tmp = mktempdir(wdir)
 
-if on_windows
-else
-   cd( tmp )#   print ( pwd() )
+# out of source-tree building:
+if !on_windows
+   cd( mktempdir(tmp) )
    ## requires NTL on host system... TODO: install it as well?
-   run(`$srcs/configure --prefix=$vdir --disable-static --disable-p-procs-static --enable-p-procs-dynamic --enable-shared --with-gmp=$vdir --with-flint=$vdir --without-python --with-readline=no --disable-gfanlib --with-debug --enable-debug --disable-optimizationflags`)
+   run(`$srcs/configure --prefix=$vdir --disable-static --disable-p-procs-static --enable-p-procs-dynamic --enable-shared --with-gmp=$vdir --with-flint=$vdir --with-ntl=$vdir --without-python --with-readline=no --disable-gfanlib --with-debug --enable-debug --disable-optimizationflags`)
    run(`make -j4`)
    run(`make install`)
    run(`rm -Rf $tmp`)
