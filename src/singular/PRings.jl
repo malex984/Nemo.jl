2d6d0dbb0595f33ea76354032f256d1a118b0759
Oleksandr Motsak <http://goo.gl/mcpzY>
2016-01-19 01:34:03 +0000

    Added ordering specification ('dp(all)') in PRing constructor
    ADD: more ring/poly related functionality (in order to pass as a Nemo Ring/RingElem)
    ADD: copy/paste flint poly tests over ZZ & QQ to be used for PRing testing...
    
    TODO: fix rSum & ring + ring!?

--- src/singular/PRings.jl
+++ src/singular/PRings.jl
@@ -8,7 +8,7 @@ type PRing <: SingularPolynomialRing
    ptr :: libSingular.ring
    base_ring :: SingularCoeffs
 
-   function PRing(cf::SingularCoeffs, _v::ASCIIString) 
+   function PRing(cf::SingularCoeffs, _v::AbstractString{}) # ASCIIString) 
       vv = split(_v, ',')
       vars = Array(AbstractString, length(vv));
       vvv = Array(Ptr{Cuchar}, length(vv))
@@ -27,14 +27,28 @@ type PRing <: SingularPolynomialRing
       end
 
 ##      println(vars)
-
+      ordering = libSingular.ringorder_dp(); # lp?
       try
-          return SRingID[cf, vars]
+          return SRingID[cf, vars, ordering]
       catch
       end
 
-      # TODO: FIXME: switch from this POC to a proper generic construction
-      ptr = r_Test(libSingular.rDefault(get_raw_ptr(cf), vvv));
+      ord_size :: Cint = 2;
+
+      ord  = pointer_to_array( 
+      	   Ptr{libSingular.rRingOrder_t}(libSingular.omAlloc0(Csize_t(ord_size * sizeof(libSingular.rRingOrder_t)))), 
+      	       (ord_size,), false );
+      blk0 = pointer_to_array( Ptr{Cint}(libSingular.omAlloc0(Csize_t(ord_size * sizeof(Cint)))), (ord_size,), false );
+      blk1 = pointer_to_array( Ptr{Cint}(libSingular.omAlloc0(Csize_t(ord_size * sizeof(Cint)))), (ord_size,), false );
+
+      ord[1] = ordering; blk0[1] = 1; blk1[1] = length(vvv); # 1st block spanns all variables...
+
+      ## the last block: everything is 0 
+#      ord[ord_size] = libSingular.ringorder_no();
+#      blk0[ord_size] = blk1[ord_size] = 0; 
+      
+
+      ptr = r_Test(libSingular.rDefault(get_raw_ptr(cf), vvv, ord, blk0, blk1));
       #@cxx test_create_ring2(get_raw_ptr(cf))
 
       (ptr == libSingular.ring(0)) && error("Singular polynomial ring construction failure")
@@ -55,7 +69,7 @@ type PRing <: SingularPolynomialRing
       (r_Test(ptr) == libSingular.ring(0)) && error("Singular polynomial ring construction failure")      
 
       try
-         R = SRingID[ptr]
+         R = SRingID[ptr] # TODO: use rVarStr etc...!?
 	 return R
       catch
       end
@@ -74,15 +88,23 @@ type PRing <: SingularPolynomialRing
    end
 end
 
-function _PRing_clear_fn(r::PRing)
-   libSingular.rDelete(get_raw_ptr(r))
-end
 
 get_raw_ptr(R :: SingularPolynomialRing) = R.ptr
 
-base_ring(R :: SingularPolynomialRing) = R.base_ring # TODO: ? verify complience! ???
+function _PRing_clear_fn(r::SingularPolynomialRing)
+   libSingular.rDelete(get_raw_ptr(r))
+end
 
 #### parent(R :: SingularPolynomialRing) = base_ring(R) # TODO: ??????!
+base_ring(R :: SingularPolynomialRing) = R.base_ring # TODO: ? verify complience! ???
+
+function +(A::SingularPolynomialRing, B::SingularPolynomialRing)
+   base_ring(A) != base_ring(B) && error("Operations on Polynomial Rings with different base-rings are not supported")
+   sum = libSingular.rSum(get_raw_ptr(A), get_raw_ptr(B))
+   return PRing(sum) 
+end
+
+
 
 #==============================================================================#
 
@@ -144,6 +166,14 @@ end
 #    PRingElem(c :: SingularPolynomialRing, s::AbstractString) = parsePRingElem(c, s) # TODO: FIXME: go via Singular?!!
 
 
+    function PRingElem(c :: SingularPolynomialRing, x::SingularCoeffsElems)
+        const CF = parent(x);
+    	( base_ring(c) != CF ) && error("Number from Incompatible Coeffs [$CF] and given Polynomial Ring [$c]")
+    	const n :: libSingular.number = libSingular.n_Copy(get_raw_ptr(x), get_raw_ptr(CF));
+	const p :: libSingular.poly   = libSingular.p_NSet(n, get_raw_ptr(c));  # NOTE: overtakes n!
+    	return PRingElem(c, p)
+    end
+
 # For now - only one element type for any Singular Polynomial Ring...
 elem_type{CF<:SingularPolynomialRing}(C::CF) = PRingElem
 
@@ -183,11 +213,18 @@ Base.call(A::SingularPolynomialRing) = elem_type(A)(A)
 Base.call(A::SingularPolynomialRing, b::Int) = elem_type(A)(A, b)
 Base.call(A::SingularPolynomialRing, b::Integer) = elem_type(A)(A, BigInt(b))
 Base.call(A::SingularPolynomialRing, b::libSingular.poly) = elem_type(A)(A, b)
+Base.call(A::SingularPolynomialRing, b::SingularCoeffsElems) = elem_type(A)(A, b)
 
 function Base.call(A::SingularPolynomialRing, b::SingularPolynomialElem)
    A != parent(b) && error("Operations on elements from different rings (mappings) are not supported yet!")
    return b ##   return deepcopy(b) # NOTE: fine no need in deepcopy!?? TODO?????
 end
+
+Base.promote_rule{S <: SingularPolynomialRing, T <: Integer}(::Type{S}, ::Type{T}) = S
+Base.promote_rule{S <: SingularPolynomialRing, T <: Integer}(::Type{T}, ::Type{S}) = S
+
+Base.promote_rule{S <: SingularPolynomialRing, T <: SingularCoeffsElems}(::Type{S}, ::Type{T}) = S
+Base.promote_rule{S <: SingularPolynomialRing, T <: SingularCoeffsElems}(::Type{T}, ::Type{S}) = S
 
 
 ###############################################################################
@@ -200,8 +237,6 @@ isring(c::SingularPolynomialRing) = true; ##libSingular.nCoeff_is_Ring(get_raw_p
 #isdomain(c::Singular?) = libSingular.nCoeff_is_Domain(get_raw_ptr(c))
 
 characteristic(r::SingularPolynomialRing) = @cxx rChar(get_raw_ptr(r))
-
-gen(r::SingularPolynomialRing) = geni( ngens(r), r) ## ??
 
 ngens(r::SingularPolynomialRing) = Int(@cxx rVar(get_raw_ptr(r)))
 npars(r::SingularPolynomialRing) = Int(@cxx rPar(get_raw_ptr(r)))
@@ -218,6 +253,20 @@ function geni(i::Int, R::SingularPolynomialRing)
     return elem_type(R)(R, p) ## PRingElem?
 end
 
+gen(r::SingularPolynomialRing) = geni( ngens(r), r) ## ??
+
+function gens(R::SingularPolynomialRing)
+    const N = ngens(R);
+    
+    vars = Array(SingularPolynomialElem, N);
+
+    for i = 1:N
+    	vars[i] = geni(i, R);	
+    end
+
+    return vars
+end
+
 ###############################################################################
 #
 #   String I/O
@@ -228,7 +277,7 @@ function string(r::SingularPolynomialRing)
    ptr = get_raw_ptr(r)
    m = @cxx rString(ptr)
    s = "SingularPolynomialRing (" * bytestring(m)  * ")" # * ", over " * string(parent(r)) ???
-   libSingular.omFree(m)
+   libSingular.omFree(Ptr{Void}(m))
 
    return s
 end
@@ -255,7 +304,7 @@ function string(p::SingularPolynomialElem)
    const R = parent(p);   
    m = libSingular.p_String(get_raw_ptr(p), get_raw_ptr(R))
    s = "[" * bytestring(m)  * "" * " over " * string(R) * "]"
-   libSingular.omFree(m)
+   libSingular.omFree(Ptr{Void}(m))
 
    return s
 end
@@ -272,6 +321,23 @@ show(io::IO, p::SingularPolynomialElem) = print(io, string(p))
         +(x::SingularPolynomialElem, i::Integer) = +(x, parent(x)(i)) 
         +(i::Integer, x::SingularPolynomialElem) = +(parent(x)(i), x)
 
+        function -(x::SingularPolynomialElem, y::SingularPolynomialElem)
+            check_parent(x, y)
+            c = parent(x)
+            p = libSingular.pp_Sub_qq(get_raw_ptr(x), get_raw_ptr(y), get_raw_ptr(c));
+            return c(p)
+        end
+        
+        -(x::SingularPolynomialElem, i::Integer) = -(x, parent(x)(i)) 
+        -(i::Integer, x::SingularPolynomialElem) = -(parent(x)(i), x)
+
+
+function -(x::SingularPolynomialElem) 
+    C = parent(x)
+    ptr = libSingular.pp_Neg( get_raw_ptr(x), get_raw_ptr(C) )
+    return C(ptr) 
+end
+
 
         function *(x::SingularPolynomialElem, y::SingularPolynomialElem)
             check_parent(x, y)
@@ -282,3 +348,32 @@ show(io::IO, p::SingularPolynomialElem) = print(io, string(p))
         
         *(x::SingularPolynomialElem, i::Integer) = *(x, parent(x)(i)) 
         *(i::Integer, x::SingularPolynomialElem) = *(parent(x)(i), x)
+
+
+
+
+###############################################################################
+#
+#   Powering
+#
+###############################################################################
+
+function ^(x::SingularPolynomialElem, y::Cint)
+    if y < 0;  throw(ErrorException("DivideError(): Negative power: " * string(y) * "!")); end
+    if isone(x); return x; end
+    if ismone(x); return isodd(y) ? x : -x; end
+##    if y > typemax(UInt); throw(ErrorException("DivideError(): Power is too big: " * string(y) * ", sorry!" )); end
+    c = parent(x)
+    if y == 0; return one(c); end
+    if y == 1; return x; end
+    p = libSingular.pp_Power(get_raw_ptr(x), y, get_raw_ptr(c))
+    return c(p)
+end
+
+
+        function isone(x :: SingularPolynomialElem)
+            return libSingular.pp_IsOne(get_raw_ptr(x), get_raw_ptr(parent(x)))
+        end
+        function ismone(x :: SingularPolynomialElem)
+            return libSingular.pp_IsMOne(get_raw_ptr(x), get_raw_ptr(parent(x)))
+        end
--- src/singular/SingularTypes.jl
+++ src/singular/SingularTypes.jl
@@ -113,163 +113,11 @@ include("NumberElem.jl")
 include("NumberCommons.jl") # Everything defined for any SingularCoeffsElems
 
 
-###############################################################################
-#
-#   SingularPolynomial (Rings)
-#
-###############################################################################
-
-# NOTE: only one sort of polynomials in Nemo, right?
-abstract SingularPolynomialRing <: Ring{Singular}
-abstract SingularPolynomialElem <: RingElem
-
-# typealias SingularRing 
-
-include("PRings.jl")
-
-# include("PRingElem.jl?")
-
-#type SingularPolynomial <: PolyElem
-
-function mullow{T <: SingularCoeffsElems}(a::PolyElem{T}, b::PolyElem{T}, n::Int)
-   check_parent(a, b)
-   lena = length(a)
-   lenb = length(b)
-
-   if lena == 0 || lenb == 0
-      return zero(parent(a))
-   end
-
-   if n < 0
-      n = 0
-   end
-
-   lenz = min(lena + lenb - 1, n)
-
-   d = zeros(T, lenz) # TODO: no real need in this intialization... :(
-
-   for i = 1:min(lena, lenz)
-      d[i] = coeff(a, i - 1)*coeff(b, 0)
-   end
-
-   if lenz > lena
-      for j = 2:min(lenb, lenz - lena + 1)
-          d[lena + j - 1] = coeff(a, lena - 1)*coeff(b, j - 1)
-      end
-   end
-
-   println("MULLOW!!!!"); # println(d);
-
-#   t = T(0)
-
-   for i = 1:lena - 1
-      if lenz > i
-         for j = 2:min(lenb, lenz - i + 1)
-
-#            mul!(t, coeff(a, i - 1), b.coeffs[j]) # TODO: FIXME: should be improved!?
-#            addeq!(d[i + j - 1], t)#?
-
-	     d[i + j - 1] += ( coeff(a, i - 1) * b.coeffs[j] )
-         end
-      end
-   end
-     
-   z = parent(a)(d)
-   
-   set_length!(z, normalise(z, lenz))
-
-   return z
-end
-
-
-function pow_multinomial{T <: SingularCoeffsElems}(a::PolyElem{T}, e::Int)
-   e < 0 && throw(DomainError())
-   lena = length(a)
-   lenz = (lena - 1) * e + 1
-   res = Array(T, lenz)
-   for k = 1:lenz
-      res[k] = base_ring(a)()
-   end
-   d = base_ring(a)()
-   first = coeff(a, 0)
-   res[1] = first ^ e
-   for k = 1 : lenz - 1
-      u = -k
-      for i = 1 : min(k, lena - 1)
-         t = coeff(a, i) * res[(k - i) + 1]
-         u += e + 1
-         addeq!(res[k + 1], t * u) ## !!!
-      end
-      addeq!(d, first) ## !!!
-      res[k + 1] = divexact(res[k + 1], d) ## ?????!
-   end
-   z = parent(a)(res)
-   set_length!(z, normalise(z, lenz))
-   return z
-end
-
-
-function ^{T <: SingularCoeffsElems}(a::PolyElem{T}, b::Int)
-   b < 0 && throw(DomainError())
-   # special case powers of x for constructing polynomials efficiently
-   if isgen(a)
-      d = Array(T, b + 1)
-      d[b + 1] = coeff(a, 1)
-      for i = 1:b
-         d[i] = coeff(a, 0)
-      end
-      z = parent(a)(d)
-      set_length!(z, b + 1)
-      return z
-   elseif length(a) == 0
-      return zero(parent(a))
-   elseif length(a) == 1
-      return parent(a)(coeff(a, 0)^b)
-   elseif b == 0
-      return one(parent(a))
-   else
-      if T <: SingularFieldElem
-         zn = 0
-         while iszero(coeff(a, zn))
-            zn += 1
-         end
-         if length(a) - zn < 8 && b > 4
-             f = shift_right(a, zn)
-             return shift_left(pow_multinomial(f, b), zn*b)  ### BUG ???
-         end
-      end
-      bit = ~((~UInt(0)) >> 1)
-      while (UInt(bit) & b) == 0
-         bit >>= 1
-      end
-      z = a
-      bit >>= 1
-      while bit != 0
-         z = z*z
-         if (UInt(bit) & b) != 0
-            z *= a
-         end
-         bit >>= 1
-      end
-      return z
-   end
-end
-
-#WARNING: New definition #is ambiguous with: 
-
-# promote_rule(Type{Nemo.Mat{#T<:Nemo.RingElem}}, Type{#T<:Nemo.RingElem})
-#      at /home/malex/.julia/v0.4/Nemo/src/generic/Matrix.jl:2357
-# promote_rule(Type{Nemo.Mat{#T<:Nemo.RingElem}}, Type{#S<:Union{Nemo.SingularRingElem, Nemo.SingularFieldElem}})
-#      at /home/malex/.julia/v0.4/Nemo/src/singular/SingularTypes.jl:225.
-#To fix, define before the new definition:
-
-# promote_rule(Type{Nemo.Mat{_<:Union{Nemo.SingularRingElem, Nemo.SingularFieldElem}}}, Type{_<:Union{Nemo.SingularRingElem, Nemo.SingularFieldElem}})
-
 Base.promote_rule{T <: SingularCoeffsElems}(::Type{Nemo.Mat{T}}, ::Type{T}) = Mat{T}
 Base.promote_rule{T <: RingElem, S <: SingularCoeffsElems}(::Type{Mat{T}}, ::Type{S}) = Mat{T}
 
-### TODO: the following will require retesting Fraction Field functionality as currently Singular_QQ and its elems are not FractionField/Elem!!! Q: how to reuse the implementation???
-
+### TODO: the following will require retesting Fraction Field functionality as currently Singular_QQ 
+### and its elems are not FractionField/Elem!!! Q: how to reuse the implementation???
 
 FractionField(::Singular_ZZ) = Singular_QQ() ## TODO: TEST ME!!!!
 
@@ -363,5 +211,25 @@ function mod(a::Singular_QQElem, z::Singular_ZZElem)
     aa = den(a)
     return Singular_QQElem(mod(num(a), aa*z), aa)
 end
+
+
+
+
+###############################################################################
+#
+#   SingularPolynomial (Rings)
+#
+###############################################################################
+
+# NOTE: only one sort of polynomials in Nemo, right?
+abstract SingularPolynomialRing <: Ring{Singular}
+abstract SingularPolynomialElem <: RingElem
+
+# typealias SingularRing 
+
+include("PRings.jl")
+
+# include("PRingElem.jl?")
+#type SingularPolynomial <: PolyElem
 
 #end
